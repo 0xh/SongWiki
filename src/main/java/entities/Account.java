87312193b3687aeeb75d78d1374de2d9872365ad
Martijn Vos <info@vosdevelopment.nl>
2019-02-25 10:50:41 +0100

    feat(global): add bean validation support for Account

--- src/main/java/entities/Account.java
+++ src/main/java/entities/Account.java
@@ -1,6 +1,10 @@
 package entities;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Entity
 @NamedQueries({
@@ -12,8 +16,15 @@ public class Account {
     @Id
     @GeneratedValue
     private int id;
+
+    @Size(min = 2, max = 40, message = "Username must be between 2 and 40 characters")
     private String username;
+
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Min(value = 13, message = "Age should not be less than 13")
+    @Max(value = 130, message = "Age should not be greater than 130")
     private int age;
 
     public int getId() { return id; }

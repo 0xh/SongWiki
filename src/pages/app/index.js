4b4917bb5a3e1e2e1ee764b9a2207f0676a109c0
Martijn Vos <info@vosdevelopment.nl>
2019-03-21 11:44:05 +0100

    feat(api): call accounts endpoint on Payara server from homepage

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    allAccounts: [],
+  };
+
+  componentDidMount() {
+    fetch('/api/accounts')
+      .then(response => response.json())
+      .then(data => this.setState({ allAccounts: data }));
+  }
+
   render() {
-    return <></>;
+    const { allAccounts } = this.state;
+    return (
+      <>
+        {allAccounts.map((account, index) => (
+          <div key={index}>
+            <h1>{account.username}</h1>
+            <p>{account.email}</p>
+            <p>{account.age}</p>
+          </div>
+        ))}
+      </>
+    );
   }
 }
 

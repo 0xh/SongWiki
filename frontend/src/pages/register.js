b35dccae3113cadac03cc95db984e867d2fff59a
Martijn Vos <info@vosdevelopment.nl>
2019-04-11 14:13:09 +0200

    F fix(redirection): fix problem redirecting to homepage after registration

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Layout from '../components/Layout';
 import RegisterForm from '../components/RegisterForm';
 
-const Register = () => (
+const Register = ({ history }) => (
   <Layout>
     <h1>Register</h1>
     <p>Get an account for SongWiki to enjoy the full experience!</p>
 
-    <RegisterForm />
+    <RegisterForm history={history} />
   </Layout>
 );
 

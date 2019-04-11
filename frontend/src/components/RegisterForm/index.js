7b1a4fd8f3eb74420abd430e223150161e566bd7
Martijn Vos <info@vosdevelopment.nl>
2019-04-11 11:16:58 +0200

    F feat(RegisterForm): componetize register form

---
+++ frontend/src/components/RegisterForm/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Formik, Form, ErrorMessage } from 'formik';
+
+import { Label, Field, Button } from './styles';
+
+const RegisterForm = ({ history }) => (
+  <Formik
+    initialValues={{
+      username: '',
+      password: '',
+      email: '',
+      age: 20,
+    }}
+    onSubmit={(values, { setSubmitting, setErrors }) => {
+      setSubmitting(true);
+
+      fetch('/api/accounts', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      })
+        .then(response => {
+          if (response.ok) {
+            setSubmitting(false);
+            history.push('/');
+          } else {
+            throw new Error(response.statusText);
+          }
+        })
+        .catch(error => {
+          setSubmitting(false);
+          // setErrors(transformMyRestApiErrorsToAnObject(error));
+          alert(error);
+        });
+    }}
+    render={({ isSubmitting }) => (
+      <Form>
+        <Label htmlFor="username">Username</Label>
+        <Field type="text" name="username" />
+        <ErrorMessage name="username" component="span" />
+
+        <Label htmlFor="password">Password</Label>
+        <Field type="password" name="password" />
+        <ErrorMessage name="password" component="span" />
+
+        <Label htmlFor="email">Email</Label>
+        <Field type="email" name="email" />
+        <ErrorMessage name="email" component="span" />
+
+        <Label htmlFor="age">Age</Label>
+        <Field type="number" name="age" />
+        <ErrorMessage name="age" component="span" />
+
+        <Button type="submit" disabled={isSubmitting}>
+          Create account
+        </Button>
+      </Form>
+    )}
+  />
+);
+
+export default RegisterForm;
--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,75 +1,15 @@
 import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
 
 import Layout from '../components/Layout';
+import RegisterForm from '../components/RegisterForm';
 
-class Register extends React.Component {
-  render() {
-    const { history } = this.props;
-    return (
-      <Layout>
-        <h1>Register</h1>
-        <p>Get an account for SongWiki to enjoy the full experience!</p>
+const Register = () => (
+  <Layout>
+    <h1>Register</h1>
+    <p>Get an account for SongWiki to enjoy the full experience!</p>
 
-        <Formik
-          initialValues={{
-            username: '',
-            password: '',
-            email: '',
-            age: 20,
-          }}
-          onSubmit={(values, { setSubmitting, setErrors }) => {
-            setSubmitting(true);
-
-            fetch('/api/accounts', {
-              method: 'POST',
-              headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(values),
-            })
-              .then(response => {
-                if (response.ok) {
-                  setSubmitting(false);
-                  history.push('/');
-                } else {
-                  throw new Error(response.statusText);
-                }
-              })
-              .catch(error => {
-                setSubmitting(false);
-                // setErrors(transformMyRestApiErrorsToAnObject(error));
-                alert(error);
-              });
-          }}
-          render={({ isSubmitting }) => (
-            <Form>
-              <label htmlFor="username">Username</label>
-              <Field type="text" name="username" />
-              <ErrorMessage name="username" component="span" />
-
-              <label htmlFor="password">Password</label>
-              <Field type="password" name="password" />
-              <ErrorMessage name="password" component="span" />
-
-              <label htmlFor="email">Email</label>
-              <Field type="email" name="email" />
-              <ErrorMessage name="email" component="span" />
-
-              <label htmlFor="age">Age</label>
-              <Field type="number" name="age" />
-              <ErrorMessage name="age" component="span" />
-
-              <button type="submit" disabled={isSubmitting}>
-                Create account
-              </button>
-            </Form>
-          )}
-        />
-      </Layout>
-    );
-  }
-}
+    <RegisterForm />
+  </Layout>
+);
 
 export default Register;

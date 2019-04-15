e5e7f2ef373a7e5a04cc4ef8dc1c57c30a5e4f18
Martijn Vos <info@vosdevelopment.nl>
2019-04-15 11:01:27 +0200

    F feat(Logout): add logout to navigation bar once logged in

--- frontend/src/components/Layout/Navigation/index.js
+++ frontend/src/components/Layout/Navigation/index.js
@@ -1,21 +1,46 @@
 import React from 'react';
 
-import { NavStyles, LinkList, LinkItem, StyledLink } from './styles';
+import {
+  NavStyles,
+  LinkList,
+  LinkItem,
+  StyledLink,
+  StyledButton,
+} from './styles';
+
+import { removeJWTToken } from '../../../utils/localStorage';
+
+import { AuthenticationConsumer } from '../../../utils/authenticationContext';
+
+const logOut = setAuthenticated => {
+  removeJWTToken();
+  setAuthenticated(false);
+};
 
 const Navigation = () => (
-  <NavStyles>
-    <h1>
-      <StyledLink to="/">SongWiki</StyledLink>
-    </h1>
-    <LinkList>
-      <LinkItem>
-        <StyledLink to="/login">Login</StyledLink>
-      </LinkItem>
-      <LinkItem hasBorder>
-        <StyledLink to="/register">Register</StyledLink>
-      </LinkItem>
-    </LinkList>
-  </NavStyles>
+  <AuthenticationConsumer>
+    {({ isAuthenticated, setAuthenticated }) => (
+      <NavStyles>
+        <h1>
+          <StyledLink to="/">SongWiki</StyledLink>
+        </h1>
+        {isAuthenticated ? (
+          <StyledButton onClick={() => logOut(setAuthenticated)}>
+            Logout
+          </StyledButton>
+        ) : (
+          <LinkList>
+            <LinkItem>
+              <StyledLink to="/login">Login</StyledLink>
+            </LinkItem>
+            <LinkItem hasBorder>
+              <StyledLink to="/register">Register</StyledLink>
+            </LinkItem>
+          </LinkList>
+        )}
+      </NavStyles>
+    )}
+  </AuthenticationConsumer>
 );
 
 export default Navigation;
--- frontend/src/components/Layout/Navigation/styles.js
+++ frontend/src/components/Layout/Navigation/styles.js
@@ -37,4 +37,19 @@ const StyledLink = styled(Link)`
   text-decoration: none;
 `;
 
-export { NavStyles, LinkList, LinkItem, StyledLink };
+const StyledButton = styled.button`
+  align-self: center;
+  border-radius: 5px;
+  padding: 7.5px 15px;
+  height: 100%;
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.theme.primaryColor};
+    color: white;
+  }
+`;
+
+export { NavStyles, LinkList, LinkItem, StyledLink, StyledButton };

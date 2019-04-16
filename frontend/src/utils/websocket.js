9c0e0a97e242c5d4dff975e248147e267f130f3f
Martijn Vos <info@vosdevelopment.nl>
2019-04-16 20:12:53 +0200

    F feat(NotificationSocket): add notification socket support

--- frontend/src/utils/websocket.js
+++ frontend/src/utils/websocket.js
@@ -1,13 +1,18 @@
+const onMessageReceived = event => {
+  const text = event.data;
+  console.log(text);
+};
+
 export const initializeWebsocket = () => {
   if (window.WebSocket) {
-    const ws = new WebSocket('ws://localhost:8080/backend/account');
-    ws.onmessage = event => {
-      const text = event.data;
-      console.log(text);
-    };
+    new WebSocket('ws://localhost:8080/backend/account').onmessage = event =>
+      onMessageReceived(event);
+
+    new WebSocket(
+      'ws://localhost:8080/backend/notification'
+    ).onmessage = event => onMessageReceived(event);
   } else {
     // Bad luck. Browser doesn't support it. Consider falling back to long polling.
     // See http://caniuse.com/websockets for an overview of supported browsers.
-    // There exist jQuery WebSocket plugins with transparent fallback.
   }
 };

e758865087ee2b5bae090cc843e75dc272cb6c56
Martijn Vos <info@vosdevelopment.nl>
2019-05-10 21:47:26 +0200

    F feat(Websockets): add playlist subscription for current user

--- frontend/src/utils/websocket.js
+++ frontend/src/utils/websocket.js
@@ -1,3 +1,6 @@
+import jwtDecode from 'jwt-decode';
+import { getJWTToken } from './localStorage';
+
 export const initializeWebsocket = setNotificationMessage => {
   if (window.WebSocket) {
     new WebSocket('ws://localhost:8080/backend/account').onmessage = event =>
@@ -5,6 +8,13 @@ export const initializeWebsocket = setNotificationMessage => {
 
     new WebSocket('ws://localhost:8080/backend/song').onmessage = event =>
       setNotificationMessage(event.data);
+
+    if (getJWTToken()) {
+      const username = jwtDecode(getJWTToken()).sub;
+      new WebSocket(
+        `ws://localhost:8080/backend/playlist/${username}`
+      ).onmessage = event => setNotificationMessage(event.data);
+    }
   } else {
     // Bad luck. Browser doesn't support it. Consider falling back to long polling.
     // See http://caniuse.com/websockets for an overview of supported browsers.
